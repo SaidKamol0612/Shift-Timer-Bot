7888de0599d00a2a22db422761b193f39bba32ba
Saidkamol <kamolsaid2006@gmail.com>
2025-09-01 15:40:41 +0500

    chore: upgrade commentaries

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -5,13 +5,6 @@ from aiogram import Router
 # Import the sub-router from handler
 from .personal import router as personal_router
 
-# -------------------------------
 # Initialize main router
-# -------------------------------
 router = Router()
-
-
-# -------------------------------
-# Include sub-router with actual handlers
-# -------------------------------
 router.include_router(personal_router)
--- src/handlers/personal/__init__.py
+++ src/handlers/personal/__init__.py
@@ -1,6 +1,3 @@
-# -------------------------------
-# Expose only 'router' from this module
-# -------------------------------
 __all__ = ("router",)
 
 from aiogram import Router
@@ -9,21 +6,13 @@ from middlewares.chat_type import PrivateChatOnlyMiddleware
 # Import the sub-router from handler
 from .handler import router as r
 
-# -------------------------------
 # Initialize main personal router
-# -------------------------------
 router = Router()
 
-# -------------------------------
 # Initialize middleware to restrict commands to private chats
-# -------------------------------
 private_only = PrivateChatOnlyMiddleware()
 
-# Apply middleware to message and callback_query handlers
+# Apply middleware to message handlers
 router.message.middleware(private_only)
-router.callback_query.middleware(private_only)
 
-# -------------------------------
-# Include sub-router with actual handlers
-# -------------------------------
 router.include_router(r)
