826d15a95840c774a875ac4e4ec436b5a56f75d2
Saidkamol <kamolsaid2006@gmail.com>
2025-09-04 11:39:34 +0500

    feat: add roles enum

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,7 @@
-__all__ = ("settings",)
+__all__ = (
+    "settings",
+    "Roles",
+)
 
 from .config import settings
+from .enums import Roles
---
+++ src/core/enums.py
@@ -0,0 +1,8 @@
+from enum import StrEnum
+
+
+class Roles(StrEnum):
+    XAMIRCHI = "xamirchi"
+    PECHKACHI = "pechkachi"
+    TERUVCHI = "teruvchi"
+    OSHPAZ = "oshpaz"
