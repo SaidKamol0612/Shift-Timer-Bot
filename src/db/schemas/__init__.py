79fb3bf80316262fbbf0e2d9e9dc024acdc2a71a
Saidkamol <kamolsaid2006@gmail.com>
2025-09-07 12:30:41 +0500

    feat: add update schema by shift_report

--- src/db/schemas/__init__.py
+++ src/db/schemas/__init__.py
@@ -1,11 +1,12 @@
 __all__ = (
     "UserSchema",
     "ShiftReportSchema",
+    "ShiftReportUpdateSchema",
     "ShiftRoleSchema",
     "RoleSchema",
 )
 
 from .user import UserSchema
-from .shift_report import ShiftReportSchema
+from .shift_report import ShiftReportSchema, ShiftReportUpdateSchema
 from .shift_role import ShiftRoleSchema
 from .role import RoleSchema
--- src/db/schemas/shift_report.py
+++ src/db/schemas/shift_report.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
-from datetime import date
+from datetime import date as PyDate
 from typing import Optional
 
 
 class ShiftReportSchema(BaseModel):
     user_id: int
-    date: date
+    date: PyDate
     start_hour: Optional[int] = None
     start_minutes: Optional[int] = None
     end_hour: Optional[int] = None
@@ -14,3 +14,17 @@ class ShiftReportSchema(BaseModel):
     pause_minutes: Optional[int] = None
     count_dough: Optional[int] = None
     shift_type: str
+    is_approved: Optional[bool] = False
+
+
+class ShiftReportUpdateSchema(BaseModel):
+    date: Optional[PyDate] = None
+    start_hour: Optional[int] = None
+    start_minutes: Optional[int] = None
+    end_hour: Optional[int] = None
+    end_minutes: Optional[int] = None
+    pause_hour: Optional[int] = None
+    pause_minutes: Optional[int] = None
+    count_dough: Optional[int] = None
+    shift_type: Optional[str] = None
+    is_approved: Optional[bool] = None
