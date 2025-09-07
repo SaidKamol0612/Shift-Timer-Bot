90411a7cc9902d7097b29febaa6bed97817964e2
Saidkamol <kamolsaid2006@gmail.com>
2025-09-07 20:59:05 +0500

    refactor: update admin util

--- src/utils/admin.py
+++ src/utils/admin.py
@@ -1,5 +1,4 @@
 import logging
-from sqlalchemy.ext.asyncio import AsyncSession
 from aiogram.types import InlineKeyboardMarkup
 
 from .load import BotLoader
@@ -10,15 +9,16 @@ log = logging.getLogger(__name__)
 class AdminUtil:
     @staticmethod
     async def send_report_to_admin(
-        text: str, reply_markup: InlineKeyboardMarkup, admin
+        text: str, reply_markup: InlineKeyboardMarkup, admin=None
     ):
-        bot = await BotLoader.get_bot()
-        try:
-            await bot.send_message(
-                chat_id=admin.tg_id, text=text, reply_markup=reply_markup
-            )
-        except Exception as e:
-            logging.warning(f"Message to admin {admin.name} failed: {e}")
+        if admin:
+            bot = await BotLoader.get_bot()
+            try:
+                await bot.send_message(
+                    chat_id=admin.tg_id, text=text, reply_markup=reply_markup
+                )
+            except Exception as e:
+                logging.warning(f"Message to admin {admin.name} failed: {e}")
 
     @staticmethod
     async def send_msg(chat_id: int, msg: str):
