f5d1fd18ebc622217f29a1279c5aafc9a7eadc1c
Saidkamol <kamolsaid2006@gmail.com>
2025-09-08 14:09:44 +0500

    refactor: rename shift model

--- src/db/models/shift.py
+++ src/db/models/shift.py
@@ -1,27 +1,31 @@
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Date, ForeignKey
+from sqlalchemy import Date, ForeignKey, Enum
+
+from core.enums import ShiftTypeENUM
 
 from .base import Base
 
 
-class ShiftReport(Base):
+class Shift(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     date = mapped_column(Date, nullable=False)
     start_hour: Mapped[int] = mapped_column(nullable=True)
-    start_minutes: Mapped[int] = mapped_column(nullable=True)
+    start_minute: Mapped[int] = mapped_column(nullable=True)
     end_hour: Mapped[int] = mapped_column(nullable=True)
-    end_minutes: Mapped[int] = mapped_column(nullable=True)
-    pause_hour: Mapped[int] = mapped_column(nullable=True)
+    end_minute: Mapped[int] = mapped_column(nullable=True)
+    pause_hours: Mapped[int] = mapped_column(nullable=True)
     pause_minutes: Mapped[int] = mapped_column(nullable=True)
     count_dough: Mapped[int] = mapped_column(nullable=True)
-    shift_type: Mapped[str] = mapped_column(nullable=False, comment="day or night")
+    shift_type: Mapped[ShiftTypeENUM] = mapped_column(
+        Enum(ShiftTypeENUM, native_enum=False), nullable=False
+    )
     is_approved: Mapped[bool] = mapped_column(nullable=False, default=False)
 
     @property
     def work_duration_minutes(self) -> int:
-        start = self.start_hour * 60 + self.start_minutes
-        end = self.end_hour * 60 + self.end_minutes
-        pause = self.pause_hour * 60 + self.pause_minutes
+        start = self.start_hour * 60 + self.start_minute
+        end = self.end_hour * 60 + self.end_minute
+        pause = self.pause_hours * 60 + self.pause_minutes
         if end < start:
             end += 24 * 60
 
