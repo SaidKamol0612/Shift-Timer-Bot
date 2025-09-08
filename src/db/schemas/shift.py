19a45d9f2c03268cfff3ef3eb3b39ba817829497
Saidkamol <kamolsaid2006@gmail.com>
2025-09-08 14:10:19 +0500

    refactor: rename shift schemas file

--- src/db/schemas/shift.py
+++ src/db/schemas/shift.py
@@ -3,21 +3,21 @@ from datetime import date as PyDate
 from typing import Optional
 
 
-class ShiftReportSchema(BaseModel):
+class ShiftSchema(BaseModel):
     user_id: int
     date: PyDate
     start_hour: Optional[int] = None
-    start_minutes: Optional[int] = None
+    start_minute: Optional[int] = None
     end_hour: Optional[int] = None
-    end_minutes: Optional[int] = None
-    pause_hour: Optional[int] = None
+    end_minute: Optional[int] = None
+    pause_hours: Optional[int] = None
     pause_minutes: Optional[int] = None
     count_dough: Optional[int] = None
     shift_type: str
     is_approved: Optional[bool] = False
 
 
-class ShiftReportUpdateSchema(BaseModel):
+class ShiftUpdateSchema(BaseModel):
     date: Optional[PyDate] = None
     start_hour: Optional[int] = None
     start_minutes: Optional[int] = None
